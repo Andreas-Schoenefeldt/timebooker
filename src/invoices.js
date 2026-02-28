ebe4f95627db2d569541422a9f5d7e04f808e78e
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2026-02-28 02:01:29 +0100

    Downloading xlsx files and reading them

--- src/invoices.js
+++ src/invoices.js
@@ -2,6 +2,7 @@ const {inquireDate} = require("./utils/date");
 const byCustomer = require("./../config/byCustomer");
 const inquirer = require("inquirer");
 const csv = require("fast-csv");
+const XLSX = require('xlsx');
 const needle = require("needle");
 const {join} = require("node:path");
 const {readFileSync, writeFileSync} = require("node:fs");
@@ -42,50 +43,57 @@ module.exports = async function() {
 
     let customers = answers.customersOrAll === 'all' ? applicableCustomers : [answers.customersOrAll];
 
+    // Read and compile the Handlebars template from the 'templates' directory
+    const templatePath = join(__dirname, '../config/templates', 'invoice.hbs');
+    const templateSource = readFileSync(templatePath, 'utf8');
+    const template = handlebars.compile(templateSource);
+
+    // Launch Puppeteer in headless mode
+    const browser = await puppeteer.launch();
+
     for (let customer of customers) {
         // thx to https://pdfbolt.com/blog/generate-pdf-nodejs-puppeteer
         console.log('Generating invoice for %o', customer);
 
         // get the data from our protected function
-        const invoiceData = await byCustomer[customer].invoiceData(start, end, {csv, needle});
-
-        console.table(invoiceData.lines);
-        console.table([
-            ['Netto:', invoiceData.netTotal.toFixed(2)],
-            ['Ust.:', invoiceData.tax.toFixed(2)],
-            ['Gesamt.:', invoiceData.total.toFixed(2)],
-        ]);
-
-        console.log('');
-
-        // Read and compile the Handlebars template from the 'templates' directory
-        const templatePath = join(__dirname, '../config/templates', 'invoice.hbs');
-        const templateSource = readFileSync(templatePath, 'utf8');
-        const template = handlebars.compile(templateSource);
-
-        const html = template(invoiceData);
-        writeFileSync(join(__dirname, '../data', invoiceData.invoiceNumber + '.html'), html);
-
-        // Launch Puppeteer in headless mode
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        // Set the generated HTML as the page content and wait for it to load completely
-        await page.setContent(html, { waitUntil: 'networkidle0' });
-
-        // Create a PDF from the HTML content and save it with a timestamped filename
-        const pdfPath =  join(__dirname, '../data', invoiceData.invoiceNumber + '.pdf');
-        await page.pdf({
-            path: pdfPath,
-            format: 'A4',
-            printBackground: true // Ensures background colors and images are included
-            // Additional parameters can be added here if needed
+        const invoiceDataArray = await byCustomer[customer].invoiceData(start, end, {
+            csv,
+            needle,
+            XLSX,
+            dataDir: join(__dirname, '../data'),
         });
 
-        // Close the browser after the PDF is generated.
-        await browser.close();
-        console.log(`PDF successfully created at: ${pdfPath}`);
+        for (let invoiceData of invoiceDataArray) {
 
+            console.table(invoiceData.lines);
+            console.table([
+                ['Netto:', invoiceData.netTotal.toFixed(2)],
+                ['Ust.:', invoiceData.tax.toFixed(2)],
+                ['Gesamt.:', invoiceData.total.toFixed(2)],
+            ]);
+
+            console.log('');
+
+            const html = template(invoiceData);
+            // writeFileSync(join(__dirname, '../data', invoiceData.invoiceNumber + '.html'), html);
+
+            // Set the generated HTML as the page content and wait for it to load completely
+            const page = await browser.newPage();
+            await page.setContent(html, { waitUntil: 'networkidle0' });
+
+            // Create a PDF from the HTML content and save it with a timestamped filename
+            const pdfPath =  join(__dirname, '../data', invoiceData.invoiceNumber + '.pdf');
+            await page.pdf({
+                path: pdfPath,
+                format: 'A4',
+                printBackground: true // Ensures background colors and images are included
+                // Additional parameters can be added here if needed
+            });
+
+            console.log(`PDF successfully created at: ${pdfPath}`);
+        }
     }
 
+    // Close the browser after the PDFs are generated.
+    await browser.close();
 }
